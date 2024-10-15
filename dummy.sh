bc42d743cf3ec7dc91cf345f544dbe2cfd413e56
gtakayama2023 <takayama@ne.phys.sci.osaka-u.ac.jp>
2024-10-16 01:07:24 +0900

    comment

--- dummy.sh
+++ dummy.sh
@@ -1,12 +1,13 @@
-cp ./LPP/ARC/20241008/BigRIPS_No01_136Xe_95Zr.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
+rm settings.db
+cp ./LPP/SHARE/BigRIPS_No01_136Xe_95Zr.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
 cp ./LPP/SHARE/BigRIPS_No02_136Xe_99Zr.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
 cp ./LPP/SHARE/BigRIPS_No03_136Xe_65Ga.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
 cp ./LPP/SHARE/BigRIPS_No04_136Xe_50V.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
 cp ./LPP/SHARE/BigRIPS_No05_136Xe_53V.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
 cp ./LPP/SHARE/BigRIPS_No06_136Xe_61Fe.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
 cp ./LPP/SHARE/BigRIPS_No07_136Xe_91Zr.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
-cp ./LPP/SHARE/BigRIPS_No09_136Xe_87Zr.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
-cp ./LPP/SHARE/BigRIPS_No08_136Xe_99Zr.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
+cp ./LPP/SHARE/BigRIPS_No08_136Xe_87Zr.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
+cp ./LPP/SHARE/BigRIPS_No09_136Xe_99Zr.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
 cp ./LPP/SHARE/BigRIPS_No10_136Xe_103Zr.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
 cp ./LPP/SHARE/BigRIPS_No11_136Xe_55V.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
 cp ./LPP/SHARE/BigRIPS_No12_136Xe_64Fe.lpp ./LPP/temp.lpp && ./SH/lise2root.sh
